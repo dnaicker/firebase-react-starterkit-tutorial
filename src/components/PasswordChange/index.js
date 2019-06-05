b97153153d6223f675171fc755a75d84972f4b7d
Denver <dnaicker@gmail.com>
2019-06-05 09:03:33 +0200

    react router display page headers

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PasswordChange = () => (
+  <div>
+    <h1>Password Change
+    </h1>
+  </div>
+);
+
+export default PasswordChange;
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PasswordForget = () => (
+  <div>
+    <h1>Password Forget
+    </h1>
+  </div>
+);
+
+export default PasswordForget;
