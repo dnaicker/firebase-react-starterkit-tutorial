947d372ee279cb8ae9e651918d32332eea7d590b
Denver <dnaicker@gmail.com>
2019-06-06 07:39:03 +0200

    constants path update and relocate

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
+import * as ROUTES from '../../constants/routes';
 
 const SignUp = () => (
   <div>
